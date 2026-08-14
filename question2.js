254e8132c156ee2a84ee24682d675bbe96fd6677
JOSEPH ADAH VINCENT <josephadahvincent@gmail.com>
2026-08-14 11:27:35 +0100

    first commit

---
+++ question2.js
@@ -0,0 +1,16 @@
+const username = 'admin'
+const password = '123456'
+if (username = ('admin')) {
+    console.log('login succesful')
+}
+else {
+    console.log('invalid username or password')
+}
+
+if (password = (123456)) {
+    console.log('login sucessful')u
+}
+else{
+    console.log('invalid username or password')
+}
+
